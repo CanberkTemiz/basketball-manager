37795fc16d2d668446c7a82d113b88ad213f979b
CanberkTemiz <canberk.tmz@gmail.com>
2023-11-27 19:29:32 +0100

    improvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from './services/user.service';
-import { Observable } from 'rxjs';
-import { SportsService } from './services/sports.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -7,12 +7,14 @@ import { SportsService } from 'src/app/services/sports.service';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
 import { TeamDetailsComponent } from './team-details/team-details.component';
+import { TeamRosterComponent } from './team-roster/team-roster.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
   styleUrls: ['./teams.component.css'],
-  imports: [NgFor, NgIf, AsyncPipe, MatDividerModule, MatListModule, TeamDetailsComponent],
+  imports: [NgFor, NgIf, AsyncPipe, RouterLink, MatDividerModule, MatListModule, TeamDetailsComponent, TeamRosterComponent],
   standalone: true
 })
 export class TeamsComponent implements OnInit {
@@ -21,6 +23,7 @@ export class TeamsComponent implements OnInit {
 
   ngOnInit(): void {
     this.teams$ = this.sportsService.getGroupByDivisionTeams()
+    this.teams$.subscribe(data => console.log(data))
   }
 
   getDivision(teams: GroupedTeams): string[] {
